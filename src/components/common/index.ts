17019dd4a864c9afd4ac89a2943b0d33221953e7
Arsh Verma <arshverma.dev@gmail.com>
2026-02-06 02:36:09 +0530

    Create index.ts

---
+++ src/components/common/index.ts
@@ -0,0 +1,5 @@
+export { default as ErrorBoundary } from './ErrorBoundary';
+export * from './Modal';
+export * from './ModalContainer';
+export { default as ScrollToTop } from './ScrollToTop';
+export * from './ThemeToggle';
